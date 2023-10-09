99df494fb31105d9d7deab04a3e6c509391af5bd
Abhinav Chawla <130582391+abhinavchawla7@users.noreply.github.com>
2023-10-09 11:52:14 +0530

    Update ATOS.py

--- ATOS.py
+++ ATOS.py
@@ -88,7 +88,7 @@ def perform_segmentation(image, selected_model):
 if selected_page == "Home":
 
     # Replace 'YOUR_API_KEY' with your actual Google Maps API key
-    google_maps_api_key = 'AIzaSyCq1_-L7ocjpoB8Sg0NaXlM-zSbjDdog2A'
+    google_maps_api_key = 'AIzaSyCOzsbWrD24xWmtix3u9hlMfRTNf6EM80w'
     # Embed the map in the Streamlit app with full width
     google_maps_component = components.v1.html(
         f"""
@@ -153,7 +153,7 @@ elif selected_page == "What we do":
 
     with tab2:
         # Create a geocoder instance
-        geolocator = GoogleV3(api_key="AIzaSyCq1_-L7ocjpoB8Sg0NaXlM-zSbjDdog2A")
+        geolocator = GoogleV3(api_key="AIzaSyCOzsbWrD24xWmtix3u9hlMfRTNf6EM80w")
         # Input for location name or coordinates
         location_input = st.text_input("Enter location name or coordinates (latitude, longitude)")
 
